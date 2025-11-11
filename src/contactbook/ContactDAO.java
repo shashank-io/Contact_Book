40ef6537426dea19c0af35ec53c448a236f558f8
Shashank K Padasalagi <padasalagishashank@gmail.com>
2025-11-11 15:07:25 +0530

    Update ContactDAO.java

--- src/contactbook/ContactDAO.java
+++ src/contactbook/ContactDAO.java
@@ -4,41 +4,29 @@ import java.sql.*;
 import java.util.*;
 
 public class ContactDAO {
-    private Connection conn = DBConnection.getConnection();
-
-    public void addContact(Contact c) throws SQLException {
+    public void addContact(Contact c) throws Exception {
+        Connection con = DBConnection.getConnection();
         String sql = "INSERT INTO contacts (name, phone, email) VALUES (?, ?, ?)";
-        PreparedStatement ps = conn.prepareStatement(sql);
+        PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, c.getName());
         ps.setString(2, c.getPhone());
         ps.setString(3, c.getEmail());
         ps.executeUpdate();
+        con.close();
     }
 
-    public List<Contact> getAllContacts() throws SQLException {
+    public List<Contact> getAllContacts() throws Exception {
         List<Contact> list = new ArrayList<>();
-        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM contacts");
+        Connection con = DBConnection.getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM contacts");
         while (rs.next()) {
-            list.add(new Contact(
-                    rs.getString("name"),
-                    rs.getString("phone"),
-                    rs.getString("email")
-            ));
+            String name = rs.getString("name");
+            String phone = rs.getString("phone");
+            String email = rs.getString("email");
+            list.add(new Contact(name, phone, email));
         }
+        con.close();
         return list;
-    }
-
-    public void deleteContact(String name) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("DELETE FROM contacts WHERE name = ?");
-        ps.setString(1, name);
-        ps.executeUpdate();
-    }
-
-    public void updateContact(String name, String newPhone, String newEmail) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("UPDATE contacts SET phone = ?, email = ? WHERE name = ?");
-        ps.setString(1, newPhone);
-        ps.setString(2, newEmail);
-        ps.setString(3, name);
-        ps.executeUpdate();
     }
 }
