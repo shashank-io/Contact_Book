9b1b96b178c38c47d892998c6c7423fd21b95b7d
Shashank K Padasalagi <padasalagishashank@gmail.com>
2025-11-11 15:05:07 +0530

    Refactor DBConnection to use constants for DB config

--- src/contactbook/DBConnection.java
+++ src/contactbook/DBConnection.java
@@ -1,18 +1,14 @@
-package contactbook;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+package contactbook;
+import java.sql.*;
 
 public class DBConnection {
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/contactdb", "root", "mysql@123"
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    private static final String URL = "jdbc:mysql://localhost:3306/contactdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "mysql@123";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
