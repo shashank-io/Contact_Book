bfe8207faced5ba08b58ff0db7e05ed18b1f5b0c
Shashank K Padasalagi <padasalagishashank@gmail.com>
2025-11-11 15:06:26 +0530

    Update Contact.java

--- src/contactbook/Contact.java
+++ src/contactbook/Contact.java
@@ -12,11 +12,6 @@ public class Contact {
     }
 
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
     public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-
     public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 }
