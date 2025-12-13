7ea04d4963597ccda3ce3504141fe4fed2088a98
Shashank K Padasalagi <padasalagishashank@gmail.com>
2025-12-13 19:29:43 +0530

    Delete src/contactbook directory

--- src/contactbook/contact.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE contacts;
-USE contacts;
-CREATE TABLE contacts (
-                          contact_id INT PRIMARY KEY AUTO_INCREMENT,
-                          name CHAR(100),
-                          phone int(10),
-                          email VARCHAR(50)
-);
